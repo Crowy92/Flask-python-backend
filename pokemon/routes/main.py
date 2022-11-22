546e116cf3c9d84a23fea1683362de6f0ca2e4d8
Tom Crowther <tom.crowther1992@gmail.comgit config --global user.name Crowy92>
2022-11-22 12:19:34 +0000

    complete :)

--- pokemon/routes/main.py
+++ pokemon/routes/main.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request, jsonify
-from ..database.pokemon import pokemons
+# from ..database.pokemon import pokemons
 from werkzeug import exceptions
 from ..models.pokemon import Pokemon
-from ..database import db
+from ..database.db import db
 
 main_routes = Blueprint("main", __name__)
 
@@ -10,8 +10,12 @@ main_routes = Blueprint("main", __name__)
 def pokemons_handler():
     if request.method == 'GET':
         pokemons = Pokemon.query.all()
+        print('***************************')
         print(pokemons)
-        return jsonify(pokemons), 200
+        outputs = map(lambda p: {"name": p.name, "frontImg": p.frontImg, "move1":p.move1}, pokemons)
+        print(outputs)
+        usableOutputs = list(outputs)
+        return jsonify(usableOutputs), 200
     elif request.method == 'POST':
         pData = request.json
         new_pokemon = Pokemon(name=pData["name"], frontImg=pData["frontImg"], move1=pData["move1"])
@@ -19,16 +23,20 @@ def pokemons_handler():
         db.session.commit()
         return jsonify(pData), 201
 
-@main_routes.route('/pokemon/<int:pokemon_id>', methods=['GET', 'DELETE'])
-def pokemon_handler(pokemon_id):
+@main_routes.route('/pokemon/<string:pokemon_name>', methods=['GET', 'DELETE'])
+def pokemon_handler(pokemon_name):
     if request.method == 'GET':
         try: 
-            return next(pokemon for pokemon in pokemons if pokemon['id'] == pokemon_id)
+            foundPoke = Pokemon.query.filter_by(name=str(pokemon_name)).first()
+            output = {"name": foundPoke.name, "frontImg": foundPoke.frontImg, "move1":foundPoke.move1}
+            return output
         except:
-            raise exceptions.BadRequest(f"We do not have a pokemon with that id: {pokemon_id}")
+            raise exceptions.BadRequest(f"We do not have a pokemon with that id: {pokemon_name}")
     elif request.method == 'DELETE':
         try:
-            del pokemons[pokemon_id -1]
-            return jsonify(pokemons), 204
+            foundPoke = Pokemon.query.filter_by(name=str(pokemon_name)).first()
+            db.session.delete(foundPoke)
+            db.session.commit()
+            return "deleted", 204
         except:
-            raise exceptions.BadRequest(f"failed to delete pokemon with htat id: {pokemon_id}")
+            raise exceptions.BadRequest(f"failed to delete pokemon with htat id: {pokemon_name}")
